6ce2a35406480e363023bdccbc23f061e267f3f2
Ethan H. Jurman <ehj2229@rit.edu>
2013-05-08 17:51:02 -0400

    added some information, still need to flesh out export

--- src/com/std/model/export/ICalFormatStrategy.java
+++ src/com/std/model/export/ICalFormatStrategy.java
@@ -1,13 +1,22 @@
 package com.std.model.export;
 
+import java.util.Date;
+import java.util.Set;
+
 import com.std.model.appointment.RefAppointment;
 
 public class ICalFormatStrategy implements ExportFormatStrategy {
 
+	// we need summary, start date, end date, date creation, Repeat
 	@Override
 	public String format(java.util.Collection<RefAppointment> appts) {
 		String outputString = "";
-		
+		for ( RefAppointment appointment : appts){
+			String summary = appointment.getDescription();
+			Date startDT = appointment.getStartDate();
+			Date endDT = appointment.getEndDate();
+			Set<Date> repeatDTs = appointment.getPattern().getDates();
+		}
 		return outputString;
 	}
 
