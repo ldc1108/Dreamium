39d15d660f0e87d1f5df85e821a154cb961aa593
Ethan H. Jurman <ehj2229@rit.edu>
2013-05-09 01:39:01 -0400

    iCal formating

--- src/com/std/model/export/ICalFormatStrategy.java
+++ src/com/std/model/export/ICalFormatStrategy.java
@@ -1,5 +1,6 @@
 package com.std.model.export;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Set;
 
@@ -7,16 +8,39 @@ import com.std.model.appointment.RefAppointment;
 
 public class ICalFormatStrategy implements ExportFormatStrategy {
 
-	// we need summary, start date, end date, date creation, Repeat
+	// we need summary, start date
+	/*
+	 * BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+BEGIN:VEVENT
+UID:uid1@example.com
+DTSTAMP:19970714T170000Z
+ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com
+DTSTART:19970714T170000Z
+DTEND:19970715T035959Z
+SUMMARY:Bastille Day Party
+END:VEVENT
+END:VCALENDAR
+RRULE:FREQ=YEARLY;INTERVAL=2;BYMONTH=1;BYDAY=SU;BYHOUR=8,9;BYMINUTE=30
+FREQ=MONTHLY;BYDAY=MO,TU,WE,TH,FR;BYSETPOS=-1
+	 */
 	@Override
 	public String format(java.util.Collection<RefAppointment> appts) {
-		String outputString = "";
+		String outputString = "BEGIN:VCALENDAR\n";
 		for ( RefAppointment appointment : appts){
-			String summary = appointment.getDescription();
-			Date startDT = appointment.getStartDate();
-			Date endDT = appointment.getEndDate();
-			Set<Date> repeatDTs = appointment.getPattern().getDates();
+			String summary = "SUMMARY:" + appointment.getTitle() + "\n";
+			for ( Date date : appointment.getPattern().getDates()){
+				outputString = outputString.concat("BEGIN:VEVENT\n");
+				String startStr = "DTSTART:" + (date.getYear()+1900) + String.format("%02d",date.getMonth()+1) + String.format("%02d",date.getDate()) + "T" + String.format("%02d",date.getHours()) + String.format("%02d",date.getMinutes()) + String.format("%02d",date.getSeconds()) + "\n";
+				String endStr = "DTEND:" + (date.getYear()+1900) + String.format("%02d",date.getMonth()+1) + String.format("%02d",date.getDate()) + "T" + String.format("%02d",appointment.getEndDate().getHours()) + String.format("%02d",appointment.getEndDate().getMinutes()) + String.format("%02d",appointment.getEndDate().getSeconds()) + "\n";
+				outputString = outputString.concat(summary);
+				outputString = outputString.concat(startStr);
+				outputString = outputString.concat(endStr);
+				outputString = outputString.concat("END:VEVENT\n");
+			}
 		}
+		outputString = outputString.concat("END:VCALENDAR\n");
 		return outputString;
 	}
 
